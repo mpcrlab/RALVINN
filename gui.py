6b470739b6b848bd1b1a08e47f4120493d2b6a45
Yunier Nieves <bleachrulez@aim.com>
2015-09-23 12:47:28 -0400

    Fixed function takeapicture Fixed error pygame.initialize() error on exit Fixed error message when x in blitscale is equal to zero.

--- gui.py
+++ gui.py
@@ -1,1 +1,20 @@
-__author__ = 'bleachrulez'
+import sys
+from PyQt4 import QtGui
+from roverShell import *
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+
+    window = QtGui.QWidget()
+    window.resize(250, 150)
+    window.move(300, 300)
+    window.setWindowTitle('Machine Perception and Cognitive Robotics')
+
+    window.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
